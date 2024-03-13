543827228bb6b68a9ddf390f93097e1e67da290a
jijijiwon <ljiwon@gmail.com>
2024-03-13 17:09:24 +0900

    java_gugudan

--- java/gugudan.java
+++ java/gugudan.java
@@ -4,14 +4,14 @@ public class gugudan {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         do {
-            system.out.print("Insert number(0:Exit) : ");
+            System.out.print("Insert number(0:Exit) : ");
             int i = s.nextInt();
 
             if(i==0){
                 System.exit(1);
             } else if (i>1 && i<10){
                 for(int j=1; j<10; j++){
-                    System.out.println(i + " * " + j " = " + i*j);
+                    System.out.println(i + " * " + j + " = " + i*j);
                 }
             } else {
                 System.out.println("Insert Number 2~9, Retype again~!!");
