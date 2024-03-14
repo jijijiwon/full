055e045e51140ce4dd1a8c41fd6907d9e7011619
jijijiwon <ljiwon0310@gmail.com>
2024-03-14 10:58:18 +0900

    primenumber

---
+++ c/gcc/quiz3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "libcheckprime.h"
+
+void main(){
+
+    while(1){
+    int x;
+    printf("\nInput integer (0:Exit) => ");
+    scanf("%d", &x);
+    if (x == 0) {
+        break;
+    }
+
+    if (checkprime(x) == x){
+        printf("%d is a prime number \n", x);
+    } else {
+        printf("%d is not prime number \n", x);
+ 
+       }   
+    }
+}
